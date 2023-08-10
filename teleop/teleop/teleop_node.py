0fb023c08d67458e34d47a7955cb4aeb202ea04b
FelipeJared <felipejaredgm@gmail.com>
2023-08-09 18:10:05 -0700

    simplified teleop

--- teleop/teleop/teleop_node.py
+++ teleop/teleop/teleop_node.py
@@ -16,40 +16,15 @@ class MinimalPublisher(Node):
         super().__init__('teleop')
 
         self.speed_setting = 2 # default to medium speed
-        self.operating_mode = 0 # 0 for Driving, 1 for Arm
+        self.operating_mode = 0 # 0 for Stopped, 1 for Driving
 
         self.subscription = self.create_subscription(
             Joy,
             'joy',
             self.on_joy,
-            10)
+            1)
         
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 1)
-        self.servo1_pub = self.create_publisher(Int32, 'joint1', 1)
-        # self.servos_pub = self.create_publisher(Int32, 'joints', 1)
-        # self.servo2 = self.create_publisher(Float64, 'joint2', 1)
-        # self.servo3 = self.create_publisher(Float64, 'joint3', 1)
-        # self.servo4 = self.create_publisher(Float64, 'joint4', 1)
-
-        self.servo1_pan_speed = 5
-        self.servo1_pan_max = 160
-        self.servo1_pan_min = 0
-        self.servo1_position = self.servo1_pan_max/2
-
-        self.servo2_pan_speed = 5
-        self.servo2_pan_max = 160
-        self.servo2_pan_min = 0
-        self.servo2_position = self.servo2_pan_max/2
-
-        self.servo3_pan_speed = 5
-        self.servo3_pan_max = 160
-        self.servo3_pan_min = 0
-        self.servo3_position = self.servo3_pan_max/2
-
-        self.servo4_pan_speed = 5
-        self.servo4_pan_max = 160
-        self.servo4_pan_min = 0
-        self.servo4_position = self.servo4_pan_max/2
 
         # self.publisher_ = self.create_publisher(String, 'topic', 10)
         # timer_period = 0.5  # seconds
@@ -80,47 +55,41 @@ class MinimalPublisher(Node):
         linear_vel  = (left_speed + right_speed) / 2.0 # (m/s)
         angular_vel  = (right_speed - left_speed) / 2.0 # (rad/s)
 
-        # Publish Twist
-        twist = Twist()
-        twist.linear.x = linear_vel
-        twist.angular.z = angular_vel
-        self.publisher_.publish(twist)
-
-        # Camera servo panning control
-        if data.buttons[4]: # pan leftward (left bumper)
-            if self.servo1_position > self.servo1_pan_min:
-                self.servo1_position -= self.servo1_pan_speed
-        if data.buttons[5]: # pan rightward (right bumper)
-            if self.servo1_position < self.servo1_pan_max:
-                self.servo1_position += self.servo1_pan_speed
-        if data.buttons[3]: # center servo position (Y button)
-            self.servo1_position = self.servo1_pan_max/2
-
-        servo1_msg = Int32()
-        servo1_msg.data = int(self.servo1_position)
-        self.servo1_pub.publish(servo1_msg)
+        # self.servo1_pub.publish(servo1_msg)
 
         # Cancel move base goal
-        if data.buttons[2]: # X button
+        # if data.buttons[2]: # X button
+        #     self.operating_mode == 0
+        #     self.get_logger().info('Stopping Movement')
+            # rospy.loginfo('Cancelling move_base goal')
+            # cancel_msg = GoalID()
+            # self.goal_cancel_pub.publish(cancel_msg)
+
+        # if data.buttons[3]: # Y button
+        #     self.operating_mode == 0
+        #     self.get_logger().info('Changed to Driving Mode')
+
+        # Cancel move base goal
+        if data.buttons[1]: # B button
+            self.operating_mode = 0
             self.get_logger().info('Stopping Movement')
             # rospy.loginfo('Cancelling move_base goal')
             # cancel_msg = GoalID()
             # self.goal_cancel_pub.publish(cancel_msg)
 
-        if data.buttons[3]: # Y button
-            if self.operating_mode == 0:
-                self.operating_mode = 1
-                self.get_logger().info('Changed to Arm Operation Mode')
-            elif self.operating_mode == 1:
-                self.operating_mode = 0
-                self.get_logger().info('Changed to Driving Mode')
-
         if data.buttons[7]: # Start button
-            self.operating_mode = 0
-            self.get_logger().info('Changed to Driving Mode')
-        elif data.buttons[6]: # Share button
             self.operating_mode = 1
-            self.get_logger().info('Changed to Arm Operation Mode')
+            self.get_logger().info('Changed to Driving Mode')
+
+        # Publish Twist
+        twist = Twist()
+        if self.operating_mode == 1:
+            twist.linear.x = linear_vel
+            twist.angular.z = angular_vel
+        else:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+        self.publisher_.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
